5388f4dff790a699f49a6ae1edb5cc5616052023
Devin Chaves <dchav90@gmail.com>
2017-08-15 15:27:15 -0400

    fetchUser refactored to async/await && action.payload states better defined

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,7 +9,9 @@ const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
-  componentDidMount() {}
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
   render() {
     return (
@@ -27,4 +29,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(null, actions)(App);
