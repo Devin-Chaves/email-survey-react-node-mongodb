64f2567df68cf4d8de4dca9fb5791a50c75518d7
Devin Chaves <dchav90@gmail.com>
2017-09-07 19:23:11 -0400

    Mailer setup

--- services/Mailer.js
+++ services/Mailer.js
@@ -2,6 +2,8 @@ const sendgrid = require('sendgrid');
 const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
-class Mailer extends helper.Mail {}
+class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {}
+}
 
 module.exports = Mailer;
